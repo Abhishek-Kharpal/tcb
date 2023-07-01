88ddb414d3eb309bedbdd1e5c9e33f251a267396
Ritik Rajdev <ritikrajdev761@gmail.com>
2023-07-01 21:20:04 +0530

    fix: remove nextAuth and Prisma

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import "~/styles/globals.css";
+import type { AppProps } from 'next/app';
+import '~/styles/globals.css';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
